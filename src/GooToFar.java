8797b4c2de2c08c39805ba565a732e5636595687
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-03-30 19:35:03 -0400

    Done I think

--- src/GooToFar.java
+++ src/GooToFar.java
@@ -2,8 +2,32 @@
 public class GooToFar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] fiveValues = {"100", "200", "300" , "400", "500"};
+		
+		try {
+			
+			for(int i = 0; i < fiveValues.length; i++)
+			{
+				System.out.println(fiveValues[i]);
+			}
+			
+			
+		} catch(ArrayIndexOutOfBoundsException e){
+			
+			throw new ArrayIndexOutOfBoundsException("Now you have gone to far\n");
+			
+		} finally {
+			
+			String finalstr = "The application is done running";
+			System.out.print(finalstr);
+		}
 
 	}
-
+	
+	public void MyE (){
+		
+		
+	}
+	
 }
